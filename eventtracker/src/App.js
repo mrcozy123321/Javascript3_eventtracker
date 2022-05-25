254e252918a348907be95bbbcdd2c09c792cdddb
Daniel Mrkoci <daniel@wiklunds.nu>
2022-05-25 22:32:34 +0200

    fixed some css

--- eventtracker/src/App.js
+++ eventtracker/src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import Views from './views/Views';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { getEvents } from './store/actions/eventsActions';
 
 
 
 function App() {
+
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(getEvents())
+  })
   return (
     <div className="App">
       <Navbar />
